0c01b42ab44128f7af54913e335819d28f45ae90
Yuliia Garreu <yuliia.garreau@gmail.com>
2025-05-15 23:01:28 +0100

    db-setup

--- src/db/models/session.js
+++ src/db/models/session.js
@@ -1,1 +1,15 @@
 // модель сесії
+import { model, Schema } from 'mongoose';
+
+const SessionsSchema = new Schema(
+  {
+    userId: { type: Schema.Types.ObjectId, ref: 'users' },
+    accessToken: { type: String, required: true },
+    refreshToken: { type: String, required: true },
+    accessTokenValidUntil: { type: Date, required: true },
+    refreshTokenValidUntil: { type: Date, required: true },
+  },
+  { timestamps: true, versionKey: false },
+);
+
+export const SessionsCollection = model('sessions', SessionsSchema);
