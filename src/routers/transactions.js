101276fc3b2b4017644f5698bd98aa57aaa9ab16
Olena_Chovgun <olenkachovgun@gmail.com>
2025-05-17 22:19:58 +0300

    update transaction

--- src/routers/transactions.js
+++ src/routers/transactions.js
@@ -1,12 +1,11 @@
 import { Router } from 'express';
 import { isValidId } from '../middlewares/isValidId.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
-import { deleteTransactionController } from '../controllers/transactions.js';
-import { authenticate } from '../middlewares/authenticate.js';
 import {
   deleteTransactionController,
   patchTransactionController,
 } from '../controllers/transactions.js';
+import { authenticate } from '../middlewares/authenticate.js';
 
 import { validateBody } from '../utils/validateBody.js';
 import { patchTransactionSchema } from '../validation/transaction.js';
@@ -22,7 +21,6 @@ transactionsRouter.use(authenticate);
 transactionsRouter.patch(
   '/:transactionId',
   isValidId,
-  //   upload.single('photo'),
   validateBody(patchTransactionSchema),
   ctrlWrapper(patchTransactionController),
 );
