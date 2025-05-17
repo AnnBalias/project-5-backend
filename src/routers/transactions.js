bcd4b12ad5680e86a0fb91556eb9d5ab53af9bda
Olena_Chovgun <olenkachovgun@gmail.com>
2025-05-17 12:48:00 +0300

    update transaction

--- src/routers/transactions.js
+++ src/routers/transactions.js
@@ -1,10 +1,22 @@
 import { Router } from 'express';
+import { isValidId } from '../middlewares/isValidId.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../utils/validateBody.js';
+import { patchTransactionSchema } from '../validation/transaction.js';
+import { patchTransactionController } from '../controllers/transaction.js';
 
 export const transactionsRouter = Router();
 
 // Rout для додавання транзакції
 
 // Rout для оновлення транзакції
+transactionsRouter.patch(
+  '/:transactionId',
+  isValidId,
+  //   upload.single('photo'),
+  validateBody(patchTransactionSchema),
+  //   ctrlWrapper(patchContactsController),
+);
 
 // Rout для видалення транзакції
 
--- src/validation/transaction.js
+++ src/validation/transaction.js
@@ -1,1 +1,18 @@
-// валідація транзакцій
+// валідація транзакцій ;
+import Joi from 'joi';
+import { categoryList } from '../constants/contacts.js';
+
+export const patchTransactionSchema = Joi.object({
+  date: Joi.string().messages({
+    // 'string.base': 'Name should be a string',
+  }),
+  type: Joi.string().messages({
+    // 'string.base': 'PhoneNumber should be a string',
+  }),
+  categoryType: Joi.string().valid(...categoryList),
+  comment: Joi.string().max(60).messages({
+    'string.base': 'comment should be a string',
+    'string.max': 'Сomment no more than 60 characters',
+  }),
+  sum: Joi.number(),
+});
