7b7bd2e94a5e54247a5d072bb1c0ecf5138b5f50
AnnBalias <annbalias34@gmail.com>
2025-05-17 12:19:02 +0300

    Update transaction.js

--- src/db/models/transaction.js
+++ src/db/models/transaction.js
@@ -1,1 +1,57 @@
-// модель транзакції
+import { Schema } from 'mongoose';
+import { model } from 'mongoose';
+import { typeList } from '../../constants/contacts.js';
+import { categoryList } from '../../constants/transactions.js';
+import { handleSaveError, setUpdateSettings } from './hooks.js';
+
+const transactionSchema = new Schema(
+  {
+    date: {
+      type: Date,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+      enum: typeList,
+      default: typeList[1],
+    },
+    category: {
+      type: String,
+      required: true,
+      enum: categoryList,
+      default: categoryList[0],
+    },
+    comment: {
+      type: String,
+      default: '',
+    },
+    sum: {
+      type: Number,
+      required: true,
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'users',
+      //   required: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+transactionSchema.post('save', handleSaveError);
+transactionSchema.pre('findOneAndUpdate', setUpdateSettings);
+transactionSchema.post('findOneAndUpdate', handleSaveError);
+
+export const transactionsSortFields = [
+  'date',
+  'type',
+  'category',
+  'comment',
+  'sum',
+];
+
+export const TransactionsCollection = model('transactions', transactionSchema);
