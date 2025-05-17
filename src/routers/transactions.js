80fba9d92778e7fec89c9570eb84b9f3d737cacc
ihor_hozhenko <lugtrauma@gmail.com>
2025-05-17 12:01:13 +0100

    Add transaction router

--- src/routers/transactions.js
+++ src/routers/transactions.js
@@ -1,11 +1,39 @@
 import { Router } from 'express';
+import { upload } from '../middlewares/upload.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
-export const transactionsRouter = Router();
+import {
+    getTransactionController,
+    getTransactionByIdController,
+    addTransactionController,
+    upsertTransactionController,
+    patchTransactionController,
+    deleteTransactionController,
+  } from '../controllers/transactions.js'; // а чи є чи може буде така папка "controllers"?
+
+import { isValidId } from '../middlewares/isValidId.js';
+import { authenticate } from '../middlewares/authenticate.js';
+
+import { validateBody } from '../utils/validateBody.js';
+
+import { contactAddSchema, contactUpdateSchema } from '../validation/transaction.js';
+
+// export const transactionsRouter = Router();
+const contactsRouter = Router();
+
+contactsRouter.use(authenticate);
+
+// Rout для отримання всіх транзакцій
+contactsRouter.get('/', ctrlWrapper(getTransactionsController));
+
+contactsRouter.get('/:id',  isValidId, ctrlWrapper(getTransactionsByIdController));
 
 // Rout для додавання транзакції
+contactsRouter.post('/', upload.single('photo'), validateBody(transactionAddSchema), ctrlWrapper(addTransactionController));
+
 
 // Rout для оновлення транзакції
 
 // Rout для видалення транзакції
 
-// Rout для отримання всіх транзакцій
+export default transactionsRouter;
