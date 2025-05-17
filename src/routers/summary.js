290fb8a28579ba82487139a9ecf9cda38f50ab28
lesia_onyshchuk <lkane443@gmail.com>
2025-05-17 19:54:02 +0300

    del authenticate

--- src/routers/summary.js
+++ src/routers/summary.js
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import { getSummary } from '../controllers/summaryController.js';
-import authMiddleware from '../middlewares/authMiddleware.js';
+// import authMiddleware from '../middlewares/authMiddleware.js';
 import { authenticate } from '../middlewares/authenticate.js';
 
 export const summaryRouter = Router();
 
 summaryRouter.use(authenticate);
 
-summaryRouter.get('/:period', authMiddleware, getSummary);
+summaryRouter.get('/:period', getSummary);
