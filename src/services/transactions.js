318eb22d1a21ffa3190c5270d200fe85625cb717
ihor_hozhenko <lugtrauma@gmail.com>
2025-05-18 11:30:28 +0100

    Correcting bugs on transactions-2

--- src/services/transactions.js
+++ src/services/transactions.js
@@ -1,51 +1,41 @@
 // сервіси для роботи з транзакціями
 import { TransactionsCollection } from '../db/models/transaction.js';
 
-
 export const getAllTransactions = async (userId) => {
-  const transactions = await TransactionsCollection.find({ userId })
+  const result = await TransactionsCollection.find({ userId })
     .sort({ date: -1, createdAt: -1 });
-  return transactions;
+  return result;
 };
 
 export const getTransactionById = async (transactionId, userId) => {
-  const transaction = await TransactionsCollection.findOne({
+  const result = await TransactionsCollection.findOne({
     _id: transactionId,
     userId
   });
-  return transaction;
+  return result;
 };
 
 export const addTransaction = async (transactionData, userId) => {
-  const transaction = await TransactionsCollection.create({
+  const result = await TransactionsCollection.create({
     ...transactionData,
     userId
   });
+  return result;
 };
 
-export const updateTransaction = async (
-  transactionId,
-  // userId,
-  payload,
-) => {
-  const transaction = await TransactionsCollection.findOneAndUpdate(
-    {
-      _id: transactionId,
-      // userId
-    },
-    payload,
-    {
-      new: true,
-    },
+export const updateTransaction = async (transactionId, updateData) => {
+  const result = await TransactionsCollection.findByIdAndUpdate(
+    transactionId,
+    updateData,
+    { new: true }
   );
-
-  return transaction;
+  return result;
 };
 
 export const deleteTransaction = async (transactionId, userId) => {
-  const transaction = await TransactionsCollection.findOneAndDelete({
+  const result = await TransactionsCollection.findOneAndDelete({
     _id: transactionId,
     userId,
   });
-  return transaction;
+  return result;
 };
