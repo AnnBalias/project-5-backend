e87d2874d21b2d88e48bc12e2002249a3adea58f
AnnBalias <annbalias@gmail.com>
2025-05-19 13:47:49 +0300

    middlewares-rev

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -1,5 +1,3 @@
-// міделвара аутентифікації
-
 import createHttpError from 'http-errors';
 import { SessionsCollection } from '../db/models/session.js';
 import { UsersCollection } from '../db/models/user.js';
@@ -13,14 +11,12 @@ export const authenticate = async (req, res, next) => {
 
   const bearer = authHeader.split(' ')[0];
   const token = authHeader.split(' ')[1];
-
   if (bearer !== 'Bearer' || !token) {
     next(createHttpError(401, 'Auth header should be of type Bearer'));
     return;
   }
 
   const session = await SessionsCollection.findOne({ accessToken: token });
-
   if (!session) {
     next(createHttpError(401, 'Session not found'));
     return;
@@ -28,14 +24,12 @@ export const authenticate = async (req, res, next) => {
 
   const isAccessTokenExpired =
     new Date() > new Date(session.accessTokenValidUntil);
-
   if (isAccessTokenExpired) {
     next(createHttpError(401, 'Access token expired'));
     return;
   }
 
   const user = await UsersCollection.findById(session.userId);
-
   if (!user) {
     next(createHttpError(401));
     return;
