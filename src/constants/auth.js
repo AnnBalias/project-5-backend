e628d99536afe0e8de534260e9344a6d80e0ecad
Viktoriia <mwa10@ukr.net>
2025-05-21 17:35:11 +0300

    add 404 for login swagger

--- src/constants/auth.js
+++ src/constants/auth.js
@@ -1,3 +1,5 @@
 export const TWO_HOURS = 2 * 60 * 60 * 1000;
 export const ONE_DAY = 24 * 60 * 60 * 1000;
 export const THIRTY_DAYS = 30 * ONE_DAY;
+
+export const FIVE_MINUTES = 5 * 60 * 1000; // 5 minutes - якщо треба буде швидко перевірити токен (змінити в services -> auth.js, де TWO_HOURS)
