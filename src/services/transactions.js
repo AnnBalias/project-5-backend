87f1c737e97dcf555306bcc376fdf9a0173dc9b3
AnnBalias <annbalias@gmail.com>
2025-05-24 14:13:29 +0300

    pagination

--- src/services/transactions.js
+++ src/services/transactions.js
@@ -1,13 +1,26 @@
 import { TransactionsCollection } from '../db/models/transaction.js';
 import { UsersCollection } from '../db/models/user.js';
 import { calculateBalanceChange } from '../utils/calculateBalanceChange.js';
+import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 
-export const getAllTransactions = async (userId) => {
-  const result = await TransactionsCollection.find({ userId }).sort({
-    date: -1,
-    createdAt: -1,
-  });
-  return result;
+export const getAllTransactions = async (userId, page, perPage) => {
+  const limit = perPage;
+  const skip = (page - 1) * perPage;
+
+  const resultCount = await TransactionsCollection.countDocuments({ userId });
+
+  const result = await TransactionsCollection.find({ userId })
+    .sort({ date: -1, createdAt: -1 })
+    .skip(skip)
+    .limit(limit)
+    .exec();
+
+  const paginationData = calculatePaginationData(resultCount, perPage, page);
+
+  return {
+    data: result,
+    ...paginationData,
+  };
 };
 
 export const addTransaction = async (transactionData, userId) => {
